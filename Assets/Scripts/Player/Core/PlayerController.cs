20de837c7401e08a6c4ad25f370f31a0c5f2bc20
Khayle Torres <khayle.torres19@imperial.ac.uk>
2022-05-15 03:12:44 +0100

    Updated world map

--- Assets/Scripts/Player/Core/PlayerController.cs
+++ Assets/Scripts/Player/Core/PlayerController.cs
@@ -18,7 +18,7 @@ namespace PlayerCore
 
         Camera cam;
         GameObject gameOverlay;
-        
+
         private void Awake() // Got changed from start to awake due to the swordColl script not finding the PlayerClass script. Awake means it runs earlier than start
         {
             DontDestroyOnLoad(this.gameObject);
