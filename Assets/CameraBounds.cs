666a567febb0985c4704b6d9ebca7a91cfd1f4ad
Khayle Torres <khayle.torres19@imperial.ac.uk>
2022-08-18 20:25:11 +0100

    Added colliders to tiles

--- Assets/CameraBounds.cs
+++ Assets/CameraBounds.cs
@@ -14,20 +14,7 @@ namespace PlayerCam
         Vector3 bottomRightCameraCoord;
         Vector3 topLeftCameraCoord;
 
-        public void setScreenRes(int W, int H)
-        {
-            Screen.SetResolution(W, H, FullScreenMode.ExclusiveFullScreen, 60);
-        }
-        private void Awake()
-        {
-            setScreenRes(640, 360);
-        }
-
-        private void LateUpdate()
-        {
-            setScreenRes(640, 360);
-            Debug.Log("Screen res low");
-        }
+        
         // Need to subscribe to scenemanager active scene changed and change the component we're trying to find for bounding updates
 
     }
